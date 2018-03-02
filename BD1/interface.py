d373171d34811754937a1da9d9c956ec67234bc0
adrianasg122 <adrianaguedes13@gmail.com>
2018-03-02 17:35:45 +0000

    Nada disto funciona... Aborta ... Abandona

---
+++ BD1/interface.py
@@ -0,0 +1,87 @@
+import mysql.connector
+
+n = 0
+
+while(1):
+
+    print("\n\n\nCriar pedido\n")
+
+    conn = mysql.connector.connect(
+        user='root',
+        password='root',
+        host='127.0.0.1',
+        database='pedido')
+
+    data = raw_input("Insira a data: ")
+    cc = raw_input("Insira o CC do utente: ")
+    tipo = raw_input("Insira o tipo pretendido para o pedido (CA,NW): ")
+    descr = raw_input("Insira a descricao: ")
+
+    cursor = conn.cursor()
+
+    try:
+        sql = "INSERT INTO pedido.Pedido(data, idDoente, tipo, descricao) VALUES (%(data)s, %(idDoente)s, %(tipo)s, %(descricao)s) "
+        cursor.execute(sql, {'data': data, 'idDoente': cc, 'tipo': tipo, 'descricao': descr, })
+        conn.commit()
+
+    except:
+        conn.rollback()
+
+
+    cursor.execute("SELECT idPedido FROM worklist LIMIT 1")
+    idP = cursor.fetchone()
+
+
+    cursor.execute("SELECT data FROM Pedido WHERE idPedido =" + str(idP[0]))
+    data = cursor.fetchone()
+
+    cursor.execute("SELECT idDoente FROM Pedido WHERE idPedido = " + str(idP[0]))
+    idD = cursor.fetchone()
+
+
+    cursor.execute("SELECT nome FROM Utente WHERE idUtenteCC = " + cc)
+    nome = cursor.fetchone()
+    nome = nome[0].encode()
+
+    cursor.execute("SELECT tipo FROM Pedido WHERE idPedido = " + str(idP[0]))
+    tipo = cursor.fetchone()
+
+
+    cursor.execute("SELECT descricao FROM Pedido WHERE idPedido = %s", (idP[0],))
+    desc = cursor.fetchone()
+
+
+    cursor.execute("SELECT sexo FROM Utente WHERE idUtenteCC = " + cc)
+    sexo = cursor.fetchone()
+    sexo = sexo[0].encode()
+
+    cursor.execute("SELECT morada FROM Utente WHERE idUtenteCC = " + cc)
+    morada = cursor.fetchone()
+    morada = morada[0].encode('ascii', 'ignore')
+
+
+    cursor.execute("SELECT telefone FROM Utente WHERE idUtenteCC = " + cc)
+    telefone = cursor.fetchone()
+
+    cursor.execute("SELECT dataNasc FROM Utente WHERE idUtenteCC = " + cc)
+    dataNasc = cursor.fetchone()
+    dataNasc = dataNasc[0].encode()
+
+    cursor.close()
+    conn.close()
+
+
+    msh = "MSH|^~\&|A|A|B|B|" + str(data[0]) + "||ORM^O01|" + str(idP[0]) + "|P|2.5||||AL\r"
+    evn = "EVN||" + str(data[0]) + "||AAA|AAA|" + str(data[0]) + "\r"
+    pid = "PID|1||" + cc + "||" + nome + "||" + str(dataNasc) + "|" + str(sexo) + "|||" + str(morada) + "||" + str(telefone[0]) + "|||" + str(sexo) + "\r"
+    pv1 = "PV1|1|I|CON|||||||||" + str(desc[0]) + "||||||||||||||||||||||||||||||||" + str(data[0]) + "\r"
+    orc = "ORC|" + str(tipo[0]) + "|" + str(data[0]) + "\r"
+    orb = "OBR|1|" + str(data[0]) + "||" + str(desc[0]) + "|||" + str(data[0]) + "\r"
+
+    aux = msh + evn + pid + pv1 + orc + orb
+    text_file = open("Output" + str(n) + ".txt", "w")
+    text_file.write(aux)
+    text_file.close()
+    n += 1
+
+exit()
