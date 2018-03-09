9ec3064a36a5ee2fb1d006c473d04e4ac4a2269e
Bruno Sousa <brunosousa_24@hotmail.com>
2018-03-09 15:47:51 +0000

    update

--- serverClient/client.py
+++ serverClient/client.py
@@ -8,7 +8,7 @@ def client():
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect the socket to the port where the server is listening
-    server_address = ('localhost', 10000)
+    server_address = ('172.20.10.3', 10000)
     print('connecting to %s port %s' % server_address)
     sock.connect(server_address)
 
