6587f82516c4cebac827529f08f346eaa6d54504
Bruno Sousa <brunosousa_24@hotmail.com>
2018-03-09 15:43:53 +0000

    update client Server

--- serverClient/client.py
+++ serverClient/client.py
@@ -1,33 +1,41 @@
 import socket
 import sys
-
-# Create a TCP/IP socket
 from pip._vendor.distlib.compat import raw_input
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print('connecting to %s port %s' % server_address)
-sock.connect(server_address)
 
-try:
+def client():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 10000)
+    print('connecting to %s port %s' % server_address)
+    sock.connect(server_address)
 
-    # Send data
-    message = raw_input("Message: ")
+    while True:
+        try:
+            message = raw_input("Message: ")
+            # Send data
+            sendData(sock,message)
+            # Look for the response
+            receiveData(sock)
+
+        finally:
+            print('closing socket')
+            sock.close()
+
+def sendData(sock,message):
     print('sending "%s"' % message)
     sock.sendall(message.encode("utf-8"))
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
 
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
+def receiveData(sock):
+    while True:
+        data = sock.recv(512)
         print('received "%s"' % data)
+        if not(data):
+            print("Finished!")
+            break
 
-finally:
-    print('closing socket')
-    sock.close()
 
+client()
--- serverClient/server.py
+++ serverClient/server.py
@@ -1,37 +1,45 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print('starting up on %s port %s' % server_address)
-sock.bind(server_address)
+def createServer():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Listen for incoming connections
-sock.listen(1)
+    # Bind the socket to the port
+    server_address = ('172.20.10.3', 10000)
+    print('starting up on %s port %s' % server_address)
+    sock.bind(server_address)
+    # Listen for incoming connections
+    sock.listen(1)
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
+    while True:
+        # Wait for a connection
+        print('waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            print('connection from', client_address)
+            # Receive the data in small chunks and retransmit it
+            data = receiveData(connection)
+            print(data)
+        finally:
+            # Clean up the connection
+            connection.close()
 
-    try:
-        print('connection from', client_address)
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print('received "%s"' % data)
-            if data:
-                print('sending data back to the client')
-                connection.sendall(data)
-            else:
-                print('no more data from', client_address)
-                break
+def sendData(sock,message):
+    print('sending "%s"' % message)
+    sock.sendall(message.encode("utf-8"))
 
-    finally:
-        # Clean up the connection
-        connection.close()
 
+def receiveData(sock):
+    data = ""
+    while True:
+        data += sock.recv(512)
+        print('received "%s"' % data)
+        if not(data):
+            print("Finished!")
+            return data
+
+
+createServer()
