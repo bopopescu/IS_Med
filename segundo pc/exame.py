55d5bc87b1d8e7571a37f02f3e12858c7849e439
Adriana Guedes <adrianaguedes13@gmail.com>
2018-03-10 00:28:45 +0000

    O 31 é um burro e não limpa lixo

--- segundo pc/exame.py
+++ segundo pc/exame.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+﻿from datetime import datetime
 import mysql.connector
 
 data = datetime.now().strftime('%Y%m%d%H%M%S')
@@ -82,37 +82,6 @@ while(1):
 
     except:
         conn.rollback()
-
-
-#cursor.execute("SELECT idPedido FROM worklist LIMIT 1")
-#idP = cursor.fetchone()
-
-#cursor.execute("SELECT data FROM Pedido WHERE idPedido =" + str(idP[0]))
-#data = cursor.fetchone()
-
-#cursor.execute("SELECT idDoente FROM Pedido WHERE idPedido = " + str(idP[0]))
-#idD = cursor.fetchone()
-
-#cursor.execute("SELECT nome FROM Utente WHERE idUtenteCC = " + str(idD[0]))
-#nome = cursor.fetchone()
-
-#cursor.execute("SELECT estado FROM Pedido WHERE idPedido = " + str(idP[0]))
-#estado = cursor.fetchone()
-
-#cursor.execute("SELECT descricao FROM Pedido WHERE idPedido = %s", (idP[0],))
-#desc = cursor.fetchone()
-
-#cursor.execute("SELECT sexo FROM Utente WHERE idUtenteCC = " + str(idD[0]))
-#sexo = cursor.fetchone()
-
-#cursor.execute("SELECT morada FROM Utente WHERE idUtenteCC = " + str(idD[0]))
-#morada = cursor.fetchone()
-
-#cursor.execute("SELECT telefone FROM Utente WHERE idUtenteCC = " + str(idD[0]))
-#telefone = cursor.fetchone()
-
-#cursor.execute("SELECT dataNasc FROM Utente WHERE idUtenteCC = " + str(idD[0]))
-#dataNasc = cursor.fetchone()
 
     cursor.close()
     conn.close()
