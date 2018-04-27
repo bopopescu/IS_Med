68ffea1f114dc338507e4244f74901b015989ec4
Bruno Sousa <brunosousa_24@hotmail.com>
2018-04-27 19:05:18 +0100

    Readme e background com bases

--- TP_Final/README.md
+++ TP_Final/README.md
@@ -1,1 +1,19 @@
+# Readme
 
+# Background
+
+### Sart background syncing process
+    python background
+
+### List ORCID being analyzed
+    python background -l
+
+### Adds ORCID('s) to be analyzed
+    python background -a 0000-0003-4121-6169
+
+    python background -a 0000-0003-4121-6168 0000-0003-4121-6169 0000-0003-4121-6170
+
+### List ORCID('s) removes ORCID from being analyzed
+    python background -r 0000-0003-4121-6169
+
+    python background -r 0000-0003-4121-6168 0000-0003-4121-6169 0000-0003-4121-6170
--- TP_Final/background.py
+++ TP_Final/background.py
@@ -1,5 +1,48 @@
 import requests
+import sys
 
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.get('https://pub.orcid.org/v2.1/0000-0003-4121-6169/works',headers=headers)
-print r.json()
+def verify(array):
+    print array
+    return True
+
+def listORCID():
+    print "-l"
+
+def addORCID(listaORCID):
+    print "-a"
+
+def remORCID(listaORCID):
+    print "-r"
+
+def instructions():
+    print "HELP:"
+    print "\tSart background syncing process:"
+    print "\t\tpython background"
+    print "\tList ORCID being analyzed:"
+    print "\t\tpython background -l"
+    print "\tAdd ORCID('s) to be analyzed:"
+    print "\t\tpython background -a XXXX-XXXX-XXXX-XXXX"
+    print "\t\tpython background -a XXXX-XXXX-XXXX-XXXX YYYY-YYYY-YYYY-YYYY ZZZZ-ZZZZ-ZZZZ-ZZZZ"
+    print "\tSRemove ORCID('s) from being analyzed:"
+    print "\t\tpython background -r XXXX-XXXX-XXXX-XXXX"
+    print "\t\tpython background -r XXXX-XXXX-XXXX-XXXX YYYY-YYYY-YYYY-YYYY ZZZZ-ZZZZ-ZZZZ-ZZZZ"
+
+def background():
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.get('https://pub.orcid.org/v2.1/0000-0003-4121-6169/works',headers=headers)
+    print r.json()
+
+
+array = sys.argv
+del array[0]
+
+if (len(array)==0):
+    background()
+elif (array[0]=="-l"):
+    listORCID()
+elif (array[0]=="-a" and verify(array)):
+    addORCID(1)
+elif (array[0]=="-r" and verify(array)):
+    remORCID(1)
+else:
+    instructions()
