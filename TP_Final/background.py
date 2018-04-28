b491a8b56301ea1662207b710582bdada828a557
Bruno Sousa <brunosousa_24@hotmail.com>
2018-04-28 17:28:50 +0100

    Inserção de campos em tabela da BD e mudança das PK's para Auto_Increment background 	lista, adiciona e remove orcid's da BD 		ao adicionar e remover verifica se os orcid's dados estão corretamente estruturados 	vai buscar artigos dos orcids da BD e eimprime o titulo

--- TP_Final/background.py
+++ TP_Final/background.py
@@ -1,18 +1,62 @@
 import requests
 import sys
+import time
+import mysql.connector
+
+conn = mysql.connector.connect(
+        user='root',
+        password='root',
+        host='127.0.0.1',
+        database='ISfinal')
+cursor = conn.cursor()
+
+
+def getOrcidsDB():
+    cursor.execute("Select orcid From Orcid;")
+    resultSet = cursor.fetchall()
+    orcidListDB = []
+    for row in resultSet:
+        orcidListDB.append(row[0].encode("utf-8"))
+    return orcidListDB
 
 def verify(array):
-    print array
+    del array[0]
+    for orcid in array:
+        if (len(orcid)!=19):
+            print "Orcid with invalid format!"
+            return False
+        for i in range(len(orcid)):
+            if (i==4 or i==9 or i==14):
+                if (orcid[i]!="-"):
+                    print "Orcid with invalid format!"
+                    return False
+            elif (not orcid[i].isdigit()):
+                print "Orcid with invalid format!"
+                return False
     return True
 
 def listORCID():
-    print "-l"
+    for orcid in orcidList:
+        print orcid
 
 def addORCID(listaORCID):
-    print "-a"
+    for orcid in listaORCID:
+        if (not orcidList.__contains__(orcid)):
+            cursor.execute("Insert into ISfinal.Orcid(orcid) values (\""+orcid+"\");")
+            conn.commit()
+            print orcid+" inserted."
+        else:
+            print orcid+" already exists."
 
 def remORCID(listaORCID):
-    print "-r"
+    for orcid in listaORCID:
+        if (orcidList.__contains__(orcid)):
+            print "Delete from Orcid where orcid=\""+orcid+"\";"
+            cursor.execute("Delete from ISfinal.Orcid where orcid=\""+orcid+"\";")
+            conn.commit()
+            print orcid+ " removed."
+        else:
+            print orcid+ " doesnt exist."
 
 def instructions():
     print "HELP:"
@@ -23,20 +67,29 @@ def instructions():
     print "\tAdd ORCID('s) to be analyzed:"
     print "\t\tpython background -a XXXX-XXXX-XXXX-XXXX"
     print "\t\tpython background -a XXXX-XXXX-XXXX-XXXX YYYY-YYYY-YYYY-YYYY ZZZZ-ZZZZ-ZZZZ-ZZZZ"
-    print "\tSRemove ORCID('s) from being analyzed:"
+    print "\tRemove ORCID('s) from being analyzed:"
     print "\t\tpython background -r XXXX-XXXX-XXXX-XXXX"
     print "\t\tpython background -r XXXX-XXXX-XXXX-XXXX YYYY-YYYY-YYYY-YYYY ZZZZ-ZZZZ-ZZZZ-ZZZZ"
 
-def background():
-    orcidList = ["0000-0003-4121-6169"]
-    for orcid in orcidList:
-        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-        r = requests.get('https://pub.orcid.org/v2.1/'+orcid+'/works',headers=headers)
-        reqJson = r.json()
-        for art in reqJson["group"]:
-            artTitle = art["work-summary"][0]["title"]["title"]["value"]
-            print orcid + " | " + artTitle
 
+def background():
+    global orcidList
+    while(True):
+        for orcid in orcidList:
+            headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+            r = requests.get('https://pub.orcid.org/v2.1/'+orcid+'/works',headers=headers)
+            reqJson = r.json()
+            for art in reqJson["group"]:
+                artTitle = art["work-summary"][0]["title"]["title"]["value"]
+                print orcid + " | " + artTitle
+        orcidList = getOrcidsDB()
+        time.sleep(5)
+
+
+
+
+
+orcidList = getOrcidsDB()
 
 array = sys.argv
 del array[0]
@@ -46,8 +99,8 @@ if (len(array)==0):
 elif (array[0]=="-l"):
     listORCID()
 elif (array[0]=="-a" and verify(array)):
-    addORCID(1)
+    addORCID(array)
 elif (array[0]=="-r" and verify(array)):
-    remORCID(1)
+    remORCID(array)
 else:
     instructions()
