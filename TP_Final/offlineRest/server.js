2bd8fda994e226442060b63f5f3de81cbca4844c
Bruno Sousa <brunosousa_24@hotmail.com>
2018-06-18 12:09:30 +0100

    fix server errors

--- TP_Final/offlineRest/server.js
+++ TP_Final/offlineRest/server.js
@@ -92,109 +92,111 @@ async function background() {
   sql = 'Select * From Orcid'
   connection.query(sql, async function (err, results) {
     if (err) throw err
-    var orcList = results;
-    var provList = []
-    var idArt = 0;
-    for (x in orcList) {
-      var request = require('request-promise');
-      await request({
-        "method": "GET",
-        "uri": "https://pub.orcid.org/v2.1/" + orcList[x].orcid + "/works",
-        "json": true,
-        "headers": {
-          "content-type": "application/json",
-          "Accept-Charset": "UTF-8"
-        }
-      }).then(reqJson => {
-        var i;
-        for (i in reqJson["group"]) {
-          var art = reqJson["group"][i];
-          var workFL = [];
-          //Verifica se alguma das referencias de um artigo tem ligacao ao scopus se tiver adiciona a lista WorkFL
-          var z;
-          var eid;
-          for (z in art["work-summary"]) {
-            var work = art["work-summary"][z];
-            var y;
-            for (y in work["external-ids"]["external-id"]) {
-              eid = work["external-ids"]["external-id"][y];
-              if (eid["external-id-type"] === "eid") {
-                workFL.push([work])
+    if (results.length !== 0) {
+      var orcList = results;
+      var provList = []
+      var idArt = 0;
+      for (x in orcList) {
+        var request = require('request-promise');
+        await request({
+          "method": "GET",
+          "uri": "https://pub.orcid.org/v2.1/" + orcList[x].orcid + "/works",
+          "json": true,
+          "headers": {
+            "content-type": "application/json",
+            "Accept-Charset": "UTF-8"
+          }
+        }).then(reqJson => {
+          var i;
+          for (i in reqJson["group"]) {
+            var art = reqJson["group"][i];
+            var workFL = [];
+            //Verifica se alguma das referencias de um artigo tem ligacao ao scopus se tiver adiciona a lista WorkFL
+            var z;
+            var eid;
+            for (z in art["work-summary"]) {
+              var work = art["work-summary"][z];
+              var y;
+              for (y in work["external-ids"]["external-id"]) {
+                eid = work["external-ids"]["external-id"][y];
+                if (eid["external-id-type"] === "eid") {
+                  workFL.push([work])
+                }
               }
             }
-          }
-          var artTitle = '';
-          var year = 0;
-          if (workFL.length === 0) {
-            //Caso nenhuma das referencias esteja ligada ao scopus apenas guarda o titulo
-            artTitle = art["work-summary"][0]["title"]["title"]["value"]
-            year = (art["work-summary"][0]["publication-date"] != null) ? art["work-summary"][0]["publication-date"]["year"]["value"] : '';
-            idArt = idArt + 1;
-            provList.push(newArt(idArt, artTitle, year, art["last-modified-date"]["value"], ''));
-          }
-          else {
-            //Caso hajam varias referencias com ligacao ao scopus analisa a que tem display-index menor
-            var workF = workFL[0]
-            var k;
-            for (k in workFL) {
-              work = workFL[k];
-              if (parseInt(work["display-index"], 10) < parseInt(workF["display-index"], 10)) {
-                workF = work
-              }
+            var artTitle = '';
+            var year = 0;
+            if (workFL.length === 0) {
+              //Caso nenhuma das referencias esteja ligada ao scopus apenas guarda o titulo
+              artTitle = art["work-summary"][0]["title"]["title"]["value"]
+              year = (art["work-summary"][0]["publication-date"] != null) ? art["work-summary"][0]["publication-date"]["year"]["value"] : '';
+              idArt = idArt + 1;
+              provList.push(newArt(idArt, artTitle, year, art["last-modified-date"]["value"], ''));
             }
-            artTitle = workF[0]["title"]["title"]["value"]
-            year = workF[0]["publication-date"]["year"]["value"]
-            var scopusID = ""
-            var it;
-            for (it in workF[0]["external-ids"]["external-id"]) {
-              eid = workF[0]["external-ids"]["external-id"][it];
-              if (eid["external-id-type"] === "eid") {
-                scopusID = eid["external-id-value"]
+            else {
+              //Caso hajam varias referencias com ligacao ao scopus analisa a que tem display-index menor
+              var workF = workFL[0]
+              var k;
+              for (k in workFL) {
+                work = workFL[k];
+                if (parseInt(work["display-index"], 10) < parseInt(workF["display-index"], 10)) {
+                  workF = work
+                }
               }
+              artTitle = workF[0]["title"]["title"]["value"]
+              year = workF[0]["publication-date"]["year"]["value"]
+              var scopusID = ""
+              var it;
+              for (it in workF[0]["external-ids"]["external-id"]) {
+                eid = workF[0]["external-ids"]["external-id"][it];
+                if (eid["external-id-type"] === "eid") {
+                  scopusID = eid["external-id-value"]
+                }
+              }
+              idArt = idArt + 1;
+              provList.push(newArt(idArt, artTitle, year, art["last-modified-date"]["value"], scopusID));
             }
-            idArt = idArt + 1;
-            provList.push(newArt(idArt, artTitle, year, art["last-modified-date"]["value"], scopusID));
           }
-        }
 
 
-        function newArt(idArt, title, year, lastModDate, scopusA) {
-          const item = {
-            idArt: idArt,
-            idOrcid: orcList[x].idOrcid,
-            orcid: orcList[x].orcid,
-            titulo: title,
-            ano: year,
-            dataModificacao: lastModDate,
-            scopus: scopusA.substr(7)
+          function newArt(idArt, title, year, lastModDate, scopusA) {
+            const item = {
+              idArt: idArt,
+              idOrcid: orcList[x].idOrcid,
+              orcid: orcList[x].orcid,
+              titulo: title,
+              ano: year,
+              dataModificacao: lastModDate,
+              scopus: scopusA.substr(7)
+            }
+            return item;
           }
-          return item;
-        }
-      });
+        });
+      }
+
+
+      var sqlOA = '';
+      var sqlA = '';
+      var i;
+      for (i in provList) {
+        sqlA = sqlA + ', (' + provList[i].idArt + ',\"' + provList[i].titulo + '\",' + ((provList[i].ano === "") ? null : provList[i].ano) + ',' + provList[i].dataModificacao + ',\"' + provList[i].scopus + '\")';
+        sqlOA = sqlOA + ', (' + provList[i].idArt + ',' + provList[i].idOrcid + ')';
+      }
+      connection.query("Delete from Orcid_has_Artigos", async function (err, results) {
+        if (err) throw err
+      })
+      connection.query("Delete from Artigos", async function (err, results) {
+        if (err) throw err
+      })
+      sqlA = 'INSERT INTO Artigos (idArtigos, titulo, ano, lastModifiedDate, scopus) VALUES' + sqlA.substr(1) + ';';
+      connection.query(sqlA, async function (err, results) {
+        if (err) throw err
+      })
+      sqlOA = 'INSERT INTO Orcid_has_Artigos (idArtigos, idOrcid) VALUES' + sqlOA.substr(1) + ';';
+      connection.query(sqlOA, async function (err, results) {
+        if (err) throw err
+      })
     }
-
-
-    var sqlOA = '';
-    var sqlA = '';
-    var i;
-    for (i in provList) {
-      sqlA = sqlA + ', (' + provList[i].idArt + ',\"' + provList[i].titulo + '\",' + ((provList[i].ano === "") ? null : provList[i].ano) + ',' + provList[i].dataModificacao + ',\"' + provList[i].scopus + '\")';
-      sqlOA = sqlOA + ', (' + provList[i].idArt + ',' + provList[i].idOrcid + ')';
-    }
-    connection.query("Delete from Orcid_has_Artigos", async function (err, results) {
-      if (err) throw err
-    })
-    connection.query("Delete from Artigos", async function (err, results) {
-      if (err) throw err
-    })
-    sqlA = 'INSERT INTO Artigos (idArtigos, titulo, ano, lastModifiedDate, scopus) VALUES' + sqlA.substr(1) + ';';
-    connection.query(sqlA, async function (err, results) {
-      if (err) throw err
-    })
-    sqlOA = 'INSERT INTO Orcid_has_Artigos (idArtigos, idOrcid) VALUES' + sqlOA.substr(1) + ';';
-    connection.query(sqlOA, async function (err, results) {
-      if (err) throw err
-    })
   })
 }
 
